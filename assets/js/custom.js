b11eb50cb8b4880bef7abed515ac5513d1899662
Aleksandr Kudriakov <clarus2137@gmail.com>
2024-01-11 00:44:48 +0100

    Anchors scrolling added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,12 +4,33 @@ function showGreetings() {
    }, 250);
 }
 
+
 function showGreetingsText() {
    setTimeout(function () {
       document.getElementById('greetings__text').classList.add('visible');
    }, 1000);
 }
 
+
+function scrollToAnchor() {
+   const anchors = document.querySelectorAll('a.nav-link[href*="#"')
+
+   for (let anchor of anchors) {
+      anchor.addEventListener('click', function(e) {
+         e.preventDefault();
+         const elementID = anchor.getAttribute('href').substr(1);
+         document.getElementById(elementID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+         });
+      });
+   }
+}
+
+
+
 showGreetings();
 
 showGreetingsText();
+
+scrollToAnchor();
